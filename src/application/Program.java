3fc2d34737e84fb5ddf5962d9efd95dbb07f5e68
Mauricio Yukio Tonooka <mytonooka@MacBook-Air-de-Mauricio.local>
2024-03-06 13:34:22 -0300

    versao 5 - aula list file-folder create folder

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,27 +1,57 @@
 package application;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.File;
+import java.util.Scanner;
 
 public class Program {
 	public static void main(String[] args) {
-		
-		String[] lines = new String[] {"Good morning","Good afternoon","Good night"};
-		
-		String home = System.getProperty("user.home");
-		String path = home + "/temp/ws-eclipse/Testing/out.txt";	
-		
-		try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) {
-			
-			for (String line : lines) {		
-				bw.write(line);
-				bw.newLine();
-			}
 
-		} catch (IOException e) {
-			e.printStackTrace();;
+		String home = System.getProperty("user.home");
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter a folder path: ");
+		String strPath = sc.nextLine();
+
+		File path = new File(home + strPath);
+		
+
+		File[] folders = path.listFiles(File::isDirectory);
+
+		System.out.println("FOLDERS:");
+
+		for (File folder : folders) {
+
+			System.out.println(folder);
+
 		}
+		
+
+		File[] files = path.listFiles(File::isFile);
+
+		System.out.println("FILES:");
+
+		for (File file : files) {
+
+			System.out.println(file);
+
+		}
+		
+		try {
+		
+		String subdir = "Subdir";
+		
+		boolean success = new File(strPath + File.separator + subdir).mkdir();
+		
+		System.out.println();
+		System.out.println("Directory created successfully: " + success);
+		
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		sc.close();
 
 	}
 }
